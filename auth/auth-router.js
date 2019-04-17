f4ee64c2d906a9031f0e4e90645c0fb4640c1c0a
Marc Thiongo <marcthuo@gmail.com>
2019-04-17 13:27:55 -0400

    first commit

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const secrets = require('../config/secrets.js');
 
 const Users = require('../users/users-model.js');
 
@@ -25,8 +27,14 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        
+        // the server needs to return the token to the clients
+        // this doesn't happen automatically like it happens with cookies
+        
         res.status(200).json({
-          message: `Welcome ${user.username}!`,
+          message: `Welcome ${user.username}!, have a token...`,
+          token
         });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -36,5 +44,17 @@ router.post('/login', (req, res) => {
       res.status(500).json(error);
     });
 });
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+    roles: ['Student'] // usually this comes from the database, won't manually add roles or etc.
+  }
+  const options = {
+    expiresIn: '1d'
+  }
+  return jwt.sign(payload, secrets.jwtSecret, options)
+}
 
 module.exports = router;
---
+++ auth/check-role-middleware.js
@@ -0,0 +1,10 @@
+module.exports = role => {
+  // return middleware
+  return function(req, res, next) {
+      if(req.decodedJwt.roles && req.decodedJwt.roles.includes(role)) {
+          next();
+      } else {
+          res.status(403).json({ you: 'have no power here!' })
+      }
+  }
+}
